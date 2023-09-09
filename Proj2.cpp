a4aa30c1e76a67206ba89ed67602fb602250d8ff
Christian Encinares Bocz <144127538+xtianencinares@users.noreply.github.com>
2023-09-09 11:11:57 +0800

    Create Proj2.cpp
    c++

---
+++ Proj2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+using namespace std;
+
+bool seePal(string word){
+    string tmp=word;
+    reverse(word.begin(),word.end());
+return tmp==word;
+     
+}
+
+    main(){
+       
+         ifstream myfile("Encinares.txt");
+         int test; myfile>>test;
+       
+       for(int a=0;a<test;a++)       
+       {
+       string word, tmp; myfile>>word;
+        int last=1; tmp = word;
+       
+        while(!seePal(word)){
+           string one="";
+         one = tmp.substr(0,last);
+      reverse(one.begin(),one.end());
+      word=tmp+one;
+      last++;               
+     }
+                cout<<word<<endl;       
+     }
+       
+       system("pause");
+}
